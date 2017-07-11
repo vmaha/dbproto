27fc19bd5ebec1047dc6ac959ce311aa42f28660
Vivek Maharajh <vivmaha@live.com>
2017-07-10 22:30:48 -0700

    refactored duplicated code into <Dashboard/>

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import { SampleData, WidgetData } from "../stores/SampleData";
+import { Header } from "../components/Header";
+import { Widget } from "../components/Widget";
+
+import "./Dashboard.scss";
+
+
+export interface Props {
+    widgets: WidgetData[];
+}
+
+export var Dashboard: React.StatelessComponent<Props> = (props: Props) => {
+
+    let widgetsElements = props.widgets.map(widgetData => <Widget {...widgetData}/>);
+
+    return (
+        <div>
+            <Header/>
+            <main>
+                <img src="/src/assets/dashboard-01.png"/>
+                { widgetsElements }
+                <img className="edit-button" src="/src/assets/dashboard-edit.png"/>
+            </main>
+        </div>
+    );
+}
--- src/views/Ellipsis.tsx
+++ src/views/Ellipsis.tsx
@@ -1,32 +1,17 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 
-import { Header } from "../components/Header";
-import { Widget } from "../components/Widget";
+import { Dashboard } from "../components/Dashboard";
 import { SampleData } from "../stores/SampleData";
 
 import "./Ellipsis.scss";
-import "file-loader!../assets/l1-l2-left.png";
-import "file-loader!../assets/l1-l2-right.png";
-import "file-loader!../assets/dashboard-01.png";
-import "file-loader!../assets/dashboard-edit.png";
 
 export var Ellipsis = (props: RouteComponentProps<any>) => {
     
     let id = props.match.params.id;
 
     let data = new SampleData();
-    let widgetsData = data.getWidgetsData(1);
-    let widgets = widgetsData.map(widgetData => <Widget {...widgetData}/>);
+    let widgets = data.getWidgetsData(1);
 
-    return (
-        <div>
-            <Header/>
-            <main>
-                <img src="/src/assets/dashboard-01.png"/>
-                { widgets }
-                <img className="edit-button" src="/src/assets/dashboard-edit.png"/>
-            </main>
-        </div>
-    );
+    return <Dashboard widgets={widgets} />;
 }
